43a0c4f0a3a88fdff5974b5b60339266fef4e19b
Hector Cordova <hectorcordova@Mac.lan>
2026-01-12 20:09:14 -0600

    Testiominal Slider feature using Flickyty

--- assets/custom-testimonials-slider.js
+++ assets/custom-testimonials-slider.js
@@ -7,3 +7,15 @@
 // infinite loop
 // the slides or cells should be aligned to the left side on initialization.
 // pauseAutoPlayOnHover: false
+
+
+// external js: flickity.pkgd.js
+const slideshow_container = document.querySelector(".testimonial-slider");
+const slider = new Flickity(slideshow_container, {
+	// options
+	wrapAround: true,
+	autoPlay: false,
+	pageDots: false,
+    pauseAutoPlayOnHover: false,
+    cellAlign: 'left'
+});
